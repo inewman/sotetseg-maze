698f7ef7f972a398d75d585163bfede7c015b1a9
inewman <inewman@users.noreply.github.com>
2019-06-01 01:52:42 -0400

    add movement logic + various improvements
    + movement logic to store tiles passed over between ticks, per tick (getPassedTiles and helper functions)
    + ability to load mazes via turning seeds (makeSeededMaze + makeSeed + editSeed)
    + remove hotkeys in lieu of html buttons calling commands directly
    + begin code for tick timing and logic (mostly commented out)
    + brief 'about' section

--- sotetseg-js/sotetseg.js
+++ sotetseg-js/sotetseg.js
@@ -1,17 +1,26 @@
+function showAbout() {
+	alert("This training tool is designed to practice movement in the Sotetseg "   +
+		"maze from the Theatre of Blood in Oldschool Runescape. You can share "    +
+		"mazes by copying the seed and copy/pasting it into the Edit Seed prompt.");
+}
+
+const tick_length  = 600;
+
 const maze_width   = 14;
 const maze_height  = 15;
 const max_x_change = 5;
 const path_turns   = 8;
 
-const tile_size   = 40;
-const tile_stroke = tile_size/25;
+const tile_size    = 40;
+const tile_stroke  = tile_size/25;
 
-const color_tileback = "#323232";
+const color_mazeback = "#323232";
 const color_tilepath = "#961919";
 const color_tilenogo = "#C8C8C8";
-const color_tileplay = "#88EE44";
+const color_tileplay = "#77dd77";
 const color_tilenext = "#C8C8C8";
-const color_tilesolv = "#DD77FF";
+const color_tilesolv = "#6495ED";
+const color_linesolv = "#FFFF00";
 
 var canvas = document.getElementById("sotetseg-maze");
 var ctx = canvas.getContext("2d");
@@ -22,12 +31,19 @@ function randRange(a, b) {
 	return Math.floor(Math.random() * (b - a + 1)) + a;
 }
 
+class Point {
+	constructor(x, y) {
+		this.x = x;
+		this.y = y;
+	}
+}
+
 function drawMazeTile(x, y, color_tile) {
 	var pos_x = tile_size * x;
 	var pos_y = tile_size * y;
 	ctx.fillStyle = color_tile;
 	ctx.fillRect(pos_x, pos_y, tile_size, tile_size);
-	ctx.fillStyle = color_tileback;
+	ctx.fillStyle = color_mazeback;
 	ctx.fillRect(
 		pos_x + tile_stroke,
 		pos_y + tile_stroke,
@@ -41,7 +57,7 @@ function drawMazeTile(x, y, color_tile) {
 	ctx.stroke();
 }
 
-function drawMaze(maze) {
+function drawMaze() {
 	for (var x = 0; x < maze.length; x++) {
 		for (var y = 0; y < maze[x].length; y++) {
 			drawMazeTile(x, y, maze[x][y] ? color_tilepath : color_tilenogo);
@@ -88,8 +104,33 @@ function makeSeed() {
 }
 
 function makeMaze() {
-	var seed = makeSeed(maze_height, maze_width, max_x_change);
+	seed = makeSeed();
 	return makeSeededMaze(seed);
+}
+
+function startMaze() {
+	maze = makeMaze();
+	drawMaze(maze);
+	moves = new Array();
+	path_taken = new Array();
+}
+
+function resetMaze() {
+	drawMaze(maze);
+	moves = new Array();
+	path_taken = new Array();
+}
+
+function connectPoints(points, color) {
+	console.log(points);
+	ctx.beginPath();
+	for (var i = 0; i < points.length - 1; i++) {
+		ctx.moveTo(points[i].x * tile_size + tile_size / 2, points[i].y * tile_size + tile_size / 2);
+		ctx.lineTo(points[i + 1].x * tile_size + tile_size / 2, points[i + 1].y * tile_size + tile_size / 2);
+	}
+	ctx.lineWidth = Math.round(tile_stroke * 1.5);
+	ctx.strokeStyle = color;
+	ctx.stroke();
 }
 
 function getTileClicked(event) {
@@ -101,35 +142,83 @@ function getTileClicked(event) {
 	return { x: tile_x, y: tile_y };
 }
 
-canvas.addEventListener('mousedown', function (event) {
-	var new_pos = getTileClicked(event);
-	drawMazeTile(new_pos.x, new_pos.y, color_tileplay);
-});
-
-canvas.addEventListener('keypress', function (event) {
-	var keyCode = event.keyCode;
-
-	console.log(keyCode);
-
-	if (keyCode == 82 || keyCode == 114){ // r
-		drawMaze(maze);
+function editSeed() {
+	var savestate = prompt("Enter a seed", seed.join(","));
+	savestate = savestate.split(',').map(Number);
+	if (savestate.length != path_turns || Math.max(savestate) >= maze_width || Math.min(savestate) < 0) {
+		alert("Bad seed");
+		return;
 	}
+	seed = savestate;
+	maze = makeSeededMaze(seed);
+	drawMaze(maze);
+}
 
-	if (keyCode == 78 || keyCode == 110){ // n
-		maze = makeMaze();
-		drawMaze(maze);
-	}
-	
-	if (keyCode == 77 || keyCode == 109){ // m
-		var str_seed = prompt("Enter a seed", seed.join(","));
-		seed = str_seed.split(',').map(Number);
-		if (seed.length < 8) {
-			seed = makeSeed();
+function getPassedTiles(previous, target) {
+	let current = new Point(previous.x, previous.y);
+	let movement_vector = new Point(target.x - current.x, target.y - current.y);
+	let turns = 0;
+	while (result.length < 2 && !(current.x == target.x && current.y == target.y)) {
+		movement_vector = new Point(target.x - current.x, target.y - current.y);
+		if (Math.abs(movement_vector.x) == Math.abs(movement_vector.y)) { // diagonal
+			current.x += (current.x < target.x ? 1 : -1);
+			current.y += (current.y < target.y ? 1 : -1);
+		} else if (Math.abs(movement_vector.x) > Math.abs(movement_vector.y)) {
+			current.x += (current.x < target.x ? 1 : -1);
+		} else {
+			current.y += (current.y < target.y ? 1 : -1);
 		}
-		maze = makeSeededMaze(seed);
-		drawMaze(maze);
+		result.push(new Point(current.x, current.y));
+		turns++;
 	}
+	return result;
+}
+
+canvas.addEventListener('mousedown', function (event) {
+	var clickedTile = getTileClicked(event);
+	let targetTile = new Point(clickedTile.x, clickedTile.y)
+	drawMazeTile(clickedTile.x, clickedTile.y, color_tilesolv);
+	if (moves.length > 0) {
+		let tiles_passed = getPassedTiles(moves[moves.length -1], targetTile);
+		for (var i = 0; i < tiles_passed.length; i++) {
+			path_taken.push(tiles_passed[i]);
+		}
+	} else {
+		path_taken.push(targetTile);
+	}
+	moves.push(targetTile);
 });
 
-var maze = makeMaze();
-drawMaze(maze);
+// function restartSession() {
+// 	endSession();
+// 	startSession();
+// }
+
+// function endSession() {
+// 	clearInterval(timerTick);
+// }
+
+// function startSession() {
+// 	ticks = 0;
+// 	session_active = true;
+// }
+
+function gameTick() {
+  ticks += 1;
+  document.getElementById("info").innerHTML = (ticks * tick_length/1000).toFixed(1) + " seconds";
+  // process movement
+}
+
+// var ticks = 0;
+// var session_active = false;
+// var timerTick = setInterval(gameTick, tick_length);
+
+
+var seed;
+var maze;
+var weighted_maze;
+var moves = new Array();
+var player_position;
+var path_taken = new Array();
+
+startMaze();
